acfe997c1d8ed2069efebcd6a8b0f1a97735f4ac
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-03-28 21:27:40 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -21,8 +21,8 @@ async def help_user(bot, update):
         
 
 
-@Client.on_message(filters.private & filters.command(["start"]))
-async def start(bot, update):
+ @Client.on_message(filters.private & filters.command(["start"]))
+ async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT,
