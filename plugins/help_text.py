1ff7983a1833b67326ccb5c51778ddf8f10b42ce
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-03-29 13:42:55 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -49,7 +49,7 @@ def about(bot, update):
     
     bot.send_message(
         chat_id=update.chat.id,
-        text=script.ABOUT_TEXT,
+        text=Translation.ABOUT_TEXT,
         parse_mode="html",
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True   
