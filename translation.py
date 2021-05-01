1a36789aad7adec64e6bd3c2b114cd435ce8c098
Predator HackerzZ <77227205+P-Phreak@users.noreply.github.com>
2021-05-01 14:10:07 +0530

    translation.py

--- translation.py
+++ translation.py
@@ -1,30 +1,52 @@
 class Translation(object):
 
-    START_TEXT = """Hello! 😎
-
-This is a Telegram Hotstar Upload Bot!
-
-<b>Please send me any Direct download URL link, I can upload to telegram as File/Video</b>
-
-/help for more details..
-
+    START_TEXT = """Hello, I am a Powefull Hotstar URL Uploader Bot 😎!
+I can support Hotstar, Google Drive, and much more Links😌!
+Send Me Any Direct Download URL Link, I Can Upload To Telegram As File/Video!
+Added Permanent Thumbnail Support💯😋!!
 Support Group : @TeleRoid14
 """
 
-    HELP_USER = """Hey! I'am a Hotstar URL Uploader bot..
+    HELP_USER = """It's not that complicated😅
     
-1. Send url (Link | New Name with Extension).
-2. Send Custom Thumbnail (Optional).
-3. Select the button.
+1. Send Me A Tumbnail if required. It'll be saved permanently.💯
+2. If Thumbnail Wasn't Set By You, It'll Be Auto Generated From The File.🥳
+3. Send Me Any Link To Be Uploaded To Telegram.
+4. Press /deletthumbnail To Delete Your Current Custom Thumbnail
+5. Select the button.
    SVideo - Give File as video with Screenshots
    DFile  - Give File with Screenshots
    Video  - Give File as video without Screenshots
    DFile  - Give File without Screenshots
-
+NB : It is Recommended To Use A Custom Thubnail Because, Some Time Wont Upload The File Without a Custom Thubnail.
 Support Group : @TeleRoid14
 """
 
-    FORMAT_SELECTION = """Select the desired format: <a href='{}'>file size might be approximate</a>
+
+    ABOUT_TEXT = """<b>🤖 My Name : @TeleRoid_Hotstar_Bot</b>
+
+<b>👨‍💻 Creator : @PredatorHackerzZ_bot</b>
+
+<b>📝 Language : Python3</b>
+
+<b>📡 Server : <a href='https://www.heroku.com'> Heroku </a></b>
+
+<b>📔 Library : Pyrogram 1.0.7</b>
+
+<b>📌 Source Code : 👉 <a href='https://github.com/P-Phreak/URLuploader-With-Hotstar'>Click Here</a></b>
+
+<b>🛑 Discussion : 👉 <a href='https://t.me/TeleRoid14'> Support Group </a></b>
+
+<b>⭕ Updates : 👉 <a href='https://t.me/TeleRoidGroup'> Channel Group </a></b>
+
+<b>💎 Telegram Botlist : <a href='https://t.me/TGRobot_List'> TGRobot_List </a></b>"""
+
+
+
+    FORMAT_SELECTION = """<b>Choose appropriate option</b> <a href='{}'>⬇️</a>
+🎞  - Stream format
+📁  - File format
+<i>NOTE : Taking high resolutions may result in files above 2GB and hence cannot Upload to TG. So better select a medium resolution.</i> 😇
     
 Send your custum thumbnail if required.
 You can use /deletethumbnail to delete the auto-generated thumbnail."""
@@ -33,7 +55,8 @@ You can use /deletethumbnail to delete the auto-generated thumbnail."""
 URL | newfilename | username | password"""
 
 
-    UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  \n\n<a href='https://github.com/P-Phreak/URLuploader-With-Hotstar'>Click here, Fork and deploy!!</a>"
+    UPGRADE_TEXT = """<b>👉 If You Liked Our Bots And Service Feel Free To Donate...🥰</b>
+  If You Want Any Private Bot Or Supscription Feel Free To Contact Me...@PredatorHackerzZ_bot...."""
     
     DOWNLOAD_START = "Trying to download.....📥"
     
@@ -45,11 +68,11 @@ URL | newfilename | username | password"""
 
     SAVED_CUSTOM_THUMB_NAIL = "Custom thumbnail saved. This will be permanent.\n\nUse /deletethumbnail to clear it."
 
-    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully."
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully.❎"
 
     CUSTOM_CAPTION_UL_FILE = " "
 
-    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+    SLOW_URL_DECED = "Fucking Fool! This seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
 
     NO_VOID_FORMAT_FOUND = "ERROR...\n<b>YouTubeDL</b> said: {}"
     
