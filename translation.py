69b54a2f1876fc8dd0240e8c978030193910a395
Predator HackerzZ <77227205+P-Phreak@users.noreply.github.com>
2021-05-17 14:16:23 +0530

    translation.py

--- translation.py
+++ translation.py
@@ -1,13 +1,13 @@
 class Translation(object):
 
-    START_TEXT = """Hello, I am a Powefull Hotstar URL Uploader Bot 😎!
-I can support Hotstar, Google Drive, and much more Links😌!
-Send Me Any Direct Download URL Link, I Can Upload To Telegram As File/Video!
-Added Permanent Thumbnail Support💯😋!!
-Support Group : @TeleRoid14
+    START_TEXT = """Hҽʅʅσ 🙋‍♂️! I αɱ α Pσɯҽϝυʅ Hσƚʂƚαɾ URL Uρʅσαԃҽɾ Bσƚ 😎!
+I ƈαɳ ʂυρρσɾƚ Hσƚʂƚαɾ, Gσσɠʅҽ Dɾιʋҽ, αɳԃ ɱυƈԋ ɱσɾҽ Lιɳƙʂ😌!
+Sҽɳԃ Mҽ Aɳყ Dιɾҽƈƚ Dσɯɳʅσαԃ URL Lιɳƙ, I Cαɳ Uρʅσαԃ Tσ Tҽʅҽɠɾαɱ Aʂ Fιʅҽ/Vιԃҽσ!
+Aԃԃҽԃ Pҽɾɱαɳҽɳƚ TԋυɱႦɳαιʅ Sυρρσɾƚ💯😋!!
+Sυρρσɾƚ Gɾσυρ : @TeleRoid14
 """
 
-    HELP_USER = """It's not that complicated😅
+    HELP_USER = """It's not that complicated to use meh! 😅
     
 1. Send Me A Tumbnail if required. It'll be saved permanently.💯
 2. If Thumbnail Wasn't Set By You, It'll Be Auto Generated From The File.🥳
@@ -18,7 +18,7 @@ Support Group : @TeleRoid14
    DFile  - Give File with Screenshots
    Video  - Give File as video without Screenshots
    DFile  - Give File without Screenshots
-NB : It is Recommended To Use A Custom Thubnail Because, Some Time Wont Upload The File Without a Custom Thubnail.
+NB : It is Recommended To Use A Custom Thumbnail Because, Some Time Won't Upload The File Without a Custom Thumbnail.
 Support Group : @TeleRoid14
 """
 
@@ -55,18 +55,18 @@ You can use /deletethumbnail to delete the auto-generated thumbnail."""
 URL | newfilename | username | password"""
 
 
-    UPGRADE_TEXT = """<b>👉 If You Liked Our Bots And Service Feel Free To Donate...🥰</b>
+    UPGRADE_TEXT = """<b>👉 If You Liked Our Bots @TheTeleRoid And Service Feel Free To Donate...Us 🥰</b>
   If You Want Any Private Bot Or Supscription Feel Free To Contact Me...@PredatorHackerzZ_bot...."""
     
-    DOWNLOAD_START = "Trying to download.....📥"
+    DOWNLOAD_START = "Trying to download to my Server, Wait For Some Time 😇\n\n@TheTeleRoid14"
     
     UPLOAD_START = "Trying to upload.....📤"
     
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds."
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds.\n\nFor TeleRoid Bots \n\nJoin : @TheTeleRoid and @TGRobot_List"
 
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nAre You Fool, I cannot upload files greater than 1.95GB due to Telegram API limitations."
 
-    SAVED_CUSTOM_THUMB_NAIL = "Custom thumbnail saved. This will be permanent.\n\nUse /deletethumbnail to clear it."
+    SAVED_CUSTOM_THUMB_NAIL = "Custom thumbnail saved✅. This will be permanent.\n\nUse /deletethumbnail to clear it."
 
     DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully.❎"
 
